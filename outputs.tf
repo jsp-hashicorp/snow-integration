40c285dea878055b1eaa0878002fca149386a066
jsp-hashicorp <jsp@hashicorp.com>
2021-09-10 01:20:58 +0900

    remve  outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "public_dns" {
-  value = aws_instance.ubuntu.public_dns
-}
+#output "public_dns" {
+#  value = aws_instance.ubuntu.public_dns
+#}
 
-output "EC2_Tag" {
-  value = [aws_instance.ubuntu.tags]
-}
+#output "EC2_Tag" {
+#  value = [aws_instance.ubuntu.tags]
+#}
 
 output "instance_type" {
   value = aws_instance.ubuntu.instance_type
